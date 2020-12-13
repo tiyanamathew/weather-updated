ddfd2203a40f679ce39e39f79756a96801a7f837
tiyanamathew <69284220+tiyanamathew@users.noreply.github.com>
2020-12-12 23:20:10 -0500

    Update date.js

--- src/date.js
+++ src/date.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Date {
+function Date() {
 let date = new Date();
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-)
+  
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
