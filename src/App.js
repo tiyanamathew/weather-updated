d6fe1f03850cf11d45e0e392c3fbcb1cc1e19761
tiyanamathew <69284220+tiyanamathew@users.noreply.github.com>
2020-12-19 23:09:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import forecastino from "./WeatherForecast.js"
-import WeatherTemperature from "./WeatherTemperature.js"
 export default function SearchEngine() {
   let [temperature, setTemperature] = useState("");
-  let [forecast, setForecast] = useState("");
+ 
   let [wind, setWind] = useState("");
   let [humidity, setHumidity] = useState("");
   const [city, setCity] = useState("");
-  
+// eslint-disable-next-line
+  const [text, setText] = useState("");
   function handleSubmit(event) {
     event.preventDefault();
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
     axios.get(url).then(showTemperature);
-    let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-    axios.get(urlForecast).then(showForecast);
+ 
   }
-  
   let today = new Date();
-  let hours = (today.getHours());
+  let hours = addZero(today.getHours());
+  let minutes = addZero(today.getMinutes());
+
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
 
 
 
   function showTemperature(response) {
-
-    setTemperature(`Currently ${Math.round(response.data.main.temp)}ºC.`);
+    setText(response.data.name);
+    setTemperature(`currently ${Math.round(response.data.main.temp)}ºC.`);
     setHumidity(response.data.main.humidity);
     setWind(response.data.wind.speed);
   }
 
-  function showForecast(response) {
-
-    setForecast(response.data.main.forecast);
-  }
 
   function updateCity(event) {
     setCity(event.target.value);
@@ -53,10 +51,6 @@ export default function SearchEngine() {
             <input type="submit" value="Search" className="btn" />
           </form>
 
-          <div className="btn-group-vertical">
-            <div className="col-2">
-            </div>
-          </div>
         </div>
       </div>
       <div className="container mt-3">
@@ -65,9 +59,7 @@ export default function SearchEngine() {
             <div className="row no-gutters">
               <div className="col-12">
                 <div className="card-body">
-                  <p id="city" className="card-text sunny its">
 
-                  </p>
                 </div>
               </div>
               <div className="col-md-6">
@@ -80,33 +72,26 @@ export default function SearchEngine() {
           <div className="col mb-3">
             <div className="card-body">
               <h2 className="its">It is...</h2>
-              <p className="sunny"> {WeatherTemperature}
-    <br />
-    {hours}h
-    </p>
-    
-
+              <p id="city" className="card-text sunny its">
+                    {temperature}
+                  </p>
               <h2 className="its" id="weather-description">
-                Humidity:
-                <span id="humidity"> {humidity}%</span> 
+                Precipitation:
+                <span id="humidity"> {humidity}</span>
                 <br />
                 Wind:
-                <span id="wind"> {wind}km/h</span>
+                <span id="wind"> {wind}</span>
               </h2>
+              <div className="card-body" id="te">
+                  <p id="date-time">Date And Time: <br /> {hours}:{minutes}</p>
+                </div>
             </div>
-   
+          </div>
         </div>
       </div>
-
-
-            <p className="card-text five">{forecastino}</p>
-          </div>
-
-     
-
-        <a id="github"> href="https://github.com/tiyanamathew/">Link to GitHub</a>
-     
-    
+      <p id="github">
+        <a href="https://github.com/tiyanamathew/weather-updated">Link to GitHub</a>
+      </p>
     </div>
-  );
+);
 }
